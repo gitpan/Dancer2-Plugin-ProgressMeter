5d3642d3914204a9bedacdd83f24a260d05d2464
Steven Humphrey <shumphrey@cpan.org>
2013-09-29 16:45:44 +0000

    Import of SHUMPHREY/Dancer2-Plugin-ProgressMeter-0.011 from CPAN.
    gitpan-cpan-distribution: Dancer2-Plugin-ProgressMeter
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         SHUMPHREY/Dancer2-Plugin-ProgressMeter-0.011.tar.gz
    gitpan-cpan-author:       SHUMPHREY
    gitpan-cpan-maturity:     released

--- lib/Dancer2/Plugin/ProgressStatus/Object.pm
+++ lib/Dancer2/Plugin/ProgressStatus/Object.pm
@@ -23,7 +23,7 @@ Steven Humphrey
 
 package Dancer2::Plugin::ProgressStatus::Object;
 {
-  $Dancer2::Plugin::ProgressStatus::Object::VERSION = '0.01';
+  $Dancer2::Plugin::ProgressStatus::Object::VERSION = '0.011';
 }
 
 use strict;
